addcd14720b6bfcb31512dc166773a502efa7387
Pratap Chandra Karmakar <pratap8122004@gmail.com>
2024-12-02 09:11:51 -0800

    wellcome massage file

---
+++ wellcome.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("\n\n");
+  //print "wellcome to my Github account."
+  printf("Wellcome to my 'Github' account.\n\n");
+
+  // #wellcome massage.
+  printf("Hello, fellow developers! \n\nwellcome to my C repository,where you'll find a collection of my projects, experiments, and coding problems.\nWhether you're here to learn, collaborate, or just explore, I'm glad to have you!");
+
+  return 0;
+}
